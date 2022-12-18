e3745c4a9105e6c2dd1d8d521c727e9f160e625e
ecyht2 <ecyht2@nottingham.edu.my>
2022-12-18 10:00:14 +0800

    Added new documentation.

--- src/mandelbrot-set/main.cpp
+++ src/mandelbrot-set/main.cpp
@@ -1,3 +1,7 @@
+/** @file main.cpp Main file containing the main function to calculates
+ * and prints a Mandelbrot set plot.
+ * */
+
 #include <iostream>
 
 #include "complex.h"
@@ -5,6 +9,12 @@
 
 using namespace std;
 
+/** The main function to print a Mandelbrot set plot.
+ *
+ * @param argc The number of arguments passed in.
+ * @param argv The arguments passed in.
+ * @return The exit status.
+ * */
 int main(int argc, char *argv[]) {
     // cw4::testComplexClass();
     cw4::Mandelbrot MBS;
